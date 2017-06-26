e1781cebf51b098b0a11cb0355923f89dfa48dd2
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-06-26 21:36:07 +0300

    Initial Commit

---
+++ swift_test_FileManager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift_test_FileManager
+//
+//  Created by Evgeniy Akhmerov on 26/06/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
