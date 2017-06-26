3847b5886dcf91b12e7681bf2ced1ab72dd05ba5
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-06-26 23:53:12 +0300

    Implemented method buttons on UI

--- swift_test_FileManager/ViewController.swift
+++ swift_test_FileManager/ViewController.swift
@@ -10,4 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - Outlets
+    
+    @IBOutlet weak var allButtonsStackView: UIStackView!
+    
+    //MARK: - Life cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+    }
+    
+    //MARK: - Private
+    
+    private func index(for button: UIButton) -> Int {
+        return allButtonsStackView.arrangedSubviews.index(of: button)!
+    }
+    
+    private func setupUI() {
+        let buttons = allButtonsStackView.arrangedSubviews
+        for (index, button) in buttons.enumerated() {
+            (button as! UIButton).setTitle(Methods(rawValue: index)!.stringValue, for: .normal)
+        }
+    }
+    
+    //MARK: - Actions
+    
+    @IBAction func onMethodCall(_ sender: UIButton) {
+    }
 }
