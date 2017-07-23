9232f9952b56587c6f7d0424992284f7af4cc903
Evgeniy Akhmerov <zzzz-w@yandex.ru>
2017-07-23 16:59:27 +0300

    Implemented string helper

---
+++ swift_test_FileManager/String+Helper.swift
@@ -0,0 +1,32 @@
+//
+//  String+Helper.swift
+//  swift_test_FileManager
+//
+//  Created by Evgeniy Akhmerov on 23/07/2017.
+//  Copyright © 2017 Evgeniy Akhmerov. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    static var oneSentenceText: String {
+        return "Integer posuere erat a ante venenatis dapibus posuere velit aliquet."
+    }
+    
+    static var twoSentencesText: String {
+        return "Sed posuere consectetur est at lobortis. Morbi leo risus, porta ac consectetur ac, vestibulum at eros."
+    }
+    
+    static var threeSentencesText: String {
+        return "Donec sed odio dui. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper."
+    }
+    
+    static var oneParagraphText: String {
+        return "Vestibulum id ligula porta felis euismod semper. Etiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas sed diam eget risus varius blandit sit amet non magna. Donec ullamcorper nulla non metus auctor fringilla. Nulla vitae elit libero, a pharetra augue."
+    }
+    
+    static var twoParagraphsText: String {
+        return "Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Vestibulum id ligula porta felis euismod semper. Etiam porta sem malesuada magna mollis euismod. Donec ullamcorper nulla non metus auctor fringilla.\n\n\tNullam quis risus eget urna mollis ornare vel eu leo. Aenean lacinia bibendum nulla sed consectetur. Maecenas faucibus mollis interdum. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue."
+    }
+}
